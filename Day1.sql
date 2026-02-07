c3e2f64aed793f8f76e99ecc8ae04e40becda2d4
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2026-02-07 20:34:35 +0600

    adding new salary table

--- Day1.sql
+++ Day1.sql
@@ -18,17 +18,71 @@ birth_date DATE
 INSERT INTO employee_demographics (employee_id, first_name, last_name, age, gender, birth_date)
 VALUES
 (1,"Lesile",'knope',44, 'Female','1979-09-25'),
-(3,'Tom', 'Haverford', 36, 'Male', '1987-03-04'),
-(4, 'April', 'Ludgate', 29, 'Female', '1994-03-27'),
-(5, 'Jerry', 'Gergich', 61, 'Male', '1962-08-28'),
-(6, 'Donna', 'Meagle', 46, 'Female', '1977-07-30'),
-(7, 'Ann', 'Perkins', 35, 'Female', '1988-12-01'),
-(8, 'Chris', 'Traeger', 43, 'Male', '1980-11-11'),
-(9, 'Ben', 'Wyatt', 38, 'Male', '1985-07-26'),
-(10, 'Andy', 'Dwyer', 34, 'Male', '1989-03-25'),
-(11, 'Mark', 'Brendanawicz', 40, 'Male', '1983-06-14'),
-(12, 'Craig', 'Middlebrooks', 37, 'Male', '1986-07-27');
+(2,'Tom', 'Haverford', 36, 'Male', '1987-03-04'),
+(3, 'April', 'Ludgate', 29, 'Female', '1994-03-27'),
+(4, 'Jerry', 'Gergich', 61, 'Male', '1962-08-28'),
+(5, 'Donna', 'Meagle', 46, 'Female', '1977-07-30'),
+(6, 'Ann', 'Perkins', 35, 'Female', '1988-12-01'),
+(7, 'Chris', 'Traeger', 43, 'Male', '1980-11-11'),
+(8, 'Ben', 'Wyatt', 38, 'Male', '1985-07-26'),
+(9, 'Andy', 'Dwyer', 34, 'Male', '1989-03-25'),
+(10, 'Mark', 'Brendanawicz', 40, 'Male', '1983-06-14'),
+(11, 'Craig', 'Middlebrooks', 37, 'Male', '1986-07-27');
 
-#ahowing the entire table
+#1.showing the entire table
 SELECT * FROM employee_demographics;
 
+#2.show only column- 2 & 4 (first_name, age)
+SELECT first_name, age FROM employee_demographics;
+
+#Show recent age and 10 years later age
+SELECT age, age+10 FROM employee_demographics;
+
+#Remove duplicates values from the Column 5 or gender
+SELECT DISTINCT gender FROM  employee_demographics;
+
+#Where Clause
+#show only Female employee
+SELECT * FROM employee_demographics
+WHERE gender = "Female";
+
+#show only male employee
+SELECT * FROM employee_demographics
+WHERE gender != "Female";
+
+#Check who born after 1985
+SELECT * FROM employee_demographics
+WHERE birth_date > "1985-01-01";
+
+#Check who born before 1985
+SELECT * FROM employee_demographics
+WHERE birth_date < "1985-01-01";
+
+#Show only male who born before 1985 (AND)
+SELECT * FROM employee_demographics
+WHERE birth_date < "1985-01-01"
+AND gender = 'Male';
+
+#Show who those who is born befor 1985 or male (OR)
+SELECT * FROM employee_demographics
+WHERE birth_date < '1985-01-01' OR gender ="Male";
+
+#Show only female those who born befor 1985 (OR NOT)
+SELECT * FROM employee_demographics
+WHERE birth_date < '1885-01-01' OR NOT gender = "Male";
+
+#LIKE Statment
+SELECT * FROM employee_demographics
+WHERE first_name LIKE "Jer%";
+
+SELECT * FROM employee_demographics
+WHERE first_name LIKE '%er%';
+
+SELECT * FROM employee_demographics
+WHERE first_name LIKE 'a%';
+
+SELECT * FROM employee_demographics
+WHERE first_name LIKE 'a__';
+
+SELECT * FROM employee_demographics
+WHERE first_name LIKE 'a__%';
---
+++ Day2.sql
@@ -0,0 +1,51 @@
+#NEW TABLE FOR SALARY
+CREATE TABLE employee_salary (
+  employee_id INT NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  occupation VARCHAR(50),
+  salary INT,
+  dept_id INT
+);
+
+INSERT INTO employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
+VALUES
+(1, 'Leslie', 'Knope', 'Deputy Director of Parks and Recreation', 75000,1),
+(2, 'Ron', 'Swanson', 'Director of Parks and Recreation', 70000,1),
+(3, 'Tom', 'Haverford', 'Entrepreneur', 50000,1),
+(4, 'April', 'Ludgate', 'Assistant to the Director of Parks and Recreation', 25000,1),
+(5, 'Jerry', 'Gergich', 'Office Manager', 50000,1),
+(6, 'Donna', 'Meagle', 'Office Manager', 60000,1),
+(7, 'Ann', 'Perkins', 'Nurse', 55000,4),
+(8, 'Chris', 'Traeger', 'City Manager', 90000,3),
+(9, 'Ben', 'Wyatt', 'State Auditor', 70000,6),
+(10, 'Andy', 'Dwyer', 'Shoe Shiner and Musician', 20000, NULL),
+(11, 'Mark', 'Brendanawicz', 'City Planner', 57000, 3),
+(12, 'Craig', 'Middlebrooks', 'Parks Director', 65000,1);
+SELECT * FROM employee_salary;
+
+#WHERE Caluse 
+
+#Check the first employee is Leslie or not
+SELECT * FROM employee_salary
+WHERE first_name = 'Leslie';
+
+#check who earns- 
+
+#more than 50000,
+SELECT * FROM employee_salary
+WHERE salary > 50000;
+
+# less then 50000,
+SELECT * FROM employee_salary
+WHERE salary < 50000;
+
+#more then equal 50000
+SELECT * FROM employee_salary
+WHERE salary >= 50000;
+
+#and less then equal 50000
+SELECT * FROM employee_salary
+WHERE salary <= 50000;
+
+
